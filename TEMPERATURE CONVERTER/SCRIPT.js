6e0aa7e3898e0b2131014e074b8b9fe6b0ca59c5
RITESH JADHAV <riteshjadhav.292005@gmail.com>
2024-07-08 23:32:58 +0530

    Add files via upload

---
+++ TEMPERATURE CONVERTER/SCRIPT.js
@@ -0,0 +1,76 @@
+function convert(){
+    var fromunit = document.getElementById('from_unit').value;
+    var tounit = document.getElementById('to_unit').value;
+    var temperature = parseFloat(document.getElementById('temperature').value);
+    var convertedvalue;
+    var resultunit;
+
+    //convertion logic
+    switch (fromunit){
+        case 'C':
+            if(tounit === 'F'){
+                convertedvalue = (temperature * 9/5) + 32;
+                resultunit = 'F';
+            } else if (tounit === 'K'){
+                convertedvalue = temperature + 273.15;
+                resultunit = 'K';
+            } else if (tounit === 'R'){
+                convertedvalue = temperature + 273.15;
+                resultunit = 'R';
+            } else{
+                convertedvalue = temperature;
+                resultunit = 'C';
+            }
+            break;
+
+            case 'F':
+            if(tounit === 'C'){
+                convertedvalue = (temperature - 32) * 5/9;
+                resultunit = 'C';
+            } else if (tounit === 'K'){
+                convertedvalue = (temperature - 32) * 5/9 + 273.15;
+                resultunit = 'K';
+            } else if (tounit === 'R'){
+                convertedvalue = temperature + 459.67;
+                resultunit = 'R';
+            } else{
+                convertedvalue = temperature;
+                resultunit = 'F';
+            }
+            break;
+
+            case 'K':
+            if(tounit === 'C'){
+                convertedvalue = temperature - 273.15;
+                resultunit = 'C';
+            } else if (tounit === 'K'){
+                convertedvalue = (temperature - 273.15) * 9/5 + 32;
+                resultunit = 'K';
+            } else if (tounit === 'R'){
+                convertedvalue = temperature * 9/5;
+                resultunit = 'R';
+            } else{
+                convertedvalue = temperature;
+                resultunit = 'K';
+            }
+            break;
+
+            case 'R':
+            if(tounit === 'C'){
+                convertedvalue = (temperature - 491.67) * 5/9;
+                resultunit = 'C';
+            } else if (tounit === 'K'){
+                convertedvalue = temperature - 459.67;
+                resultunit = 'K';
+            } else if (tounit === 'R'){
+                convertedvalue = temperature * 5/9;
+                resultunit = 'R';
+            } else{
+                convertedvalue = temperature;
+                resultunit = 'R';
+            }
+            break;
+    }
+
+    document.getElementById('result').value = convertedvalue.toFixed(2) + ' ' + resultunit;
+}
